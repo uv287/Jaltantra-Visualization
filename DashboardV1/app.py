8fc894d6dc50bfa1789bd18840abbecdc51cdab7
uv287 <utsavmananiuv@gmail.com>
2025-06-19 11:18:32 +0530

    server accessible Update

--- DashboardV1/app.py
+++ DashboardV1/app.py
@@ -13,4 +13,4 @@ app.layout = Layout().create_layout()
 register_callbacks(app)
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0', port=8050)
